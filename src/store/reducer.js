f6baa468322487cc692e6e7703c16290ca1776c0
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-06-08 22:01:07 +0530

    2024_6_8

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import customizationReducer from './customizationReducer';
 import snackbarReducer from './snackbarReducer';
-import musicPlayerReducer from './musicPlayerReducer '
+import musicPlayerReducer from './musicPlayerReducer';
 
 const reducer = combineReducers({
     customization: customizationReducer,
