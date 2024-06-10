a73a0e631dc42ca145f7f9249c10d998ed8db5d2
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-06-10 14:35:31 +0530

    2024_

--- src/themes/logo.js
+++ src/themes/logo.js
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import constant from './constant';
+import logo from '../assets/images/music_ai_logo.png'
 
 const useStyles = makeStyles({
   root: {
@@ -15,9 +16,9 @@ const Logo = () => {
     const classes = useStyles()
 
   return (
-    <Typography className={classes.root} variant="h2" gutterBottom>
-            MUSIC.AI
-      </Typography>
+    // <Typography className={classes.root} variant="h2" gutterBottom>
+            <img src={logo} alt='logo' width='150px' />
+      // </Typography>
   )
 }
 
