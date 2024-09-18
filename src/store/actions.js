c83f1872ceefdd6b159ea5f0dd4004a3566419bf
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-18 12:00:36 +0530

    music_player

--- src/store/actions.js
+++ src/store/actions.js
@@ -19,21 +19,24 @@ export const SNACKBAR_OPEN = '@snackbar/SNACKBAR_OPEN';
 export const SHOW_MUSIC_PLAYER = 'SHOW_MUSIC_PLAYER';
 export const HIDE_MUSIC_PLAYER = 'HIDE_MUSIC_PLAYER';
 
-export const showMusicPlayer = () => ({
-    type: SHOW_MUSIC_PLAYER,
-  });
-  
-  export const hideMusicPlayer = () => ({
-    type: HIDE_MUSIC_PLAYER,
-  });
+export const showMusicPlayer = (song) => ({
+  type: SHOW_MUSIC_PLAYER,
+  payload: song,  // Pass the song/item object to the player when dispatching
+});
 
+export const hideMusicPlayer = () => ({
+  type: HIDE_MUSIC_PLAYER,
+});
 
-  // User
-  export const setUser = (user) => ({
-    type: 'SET_USER',
-    payload: user,
+// User Actions
+export const SET_USER = 'SET_USER';
+export const CLEAR_USER = 'CLEAR_USER';
+
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user,
 });
 
 export const clearUser = () => ({
-    type: 'CLEAR_USER',
+  type: CLEAR_USER,
 });
--- src/store/musicPlayerReducer.js
+++ src/store/musicPlayerReducer.js
@@ -1,21 +1,28 @@
 import { SHOW_MUSIC_PLAYER, HIDE_MUSIC_PLAYER } from './actions';
 
+// Initial state of the music player
 const initialState = {
-  isVisible: false,
+  isVisible: false,  // Determines if the music player is visible or hidden
 };
 
+// Music player reducer function
 const musicPlayerReducer = (state = initialState, action) => {
   switch (action.type) {
+    // Action to show the music player
     case SHOW_MUSIC_PLAYER:
       return {
         ...state,
-        isVisible: true,
+        isVisible: true,  // Set visibility to true
       };
+      
+    // Action to hide the music player
     case HIDE_MUSIC_PLAYER:
       return {
         ...state,
-        isVisible: false,
+        isVisible: false,  // Set visibility to false
       };
+      
+    // Return the current state by default
     default:
       return state;
   }
