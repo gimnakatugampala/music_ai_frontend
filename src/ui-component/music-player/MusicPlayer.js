8c73aea9bcb7fda41bb2c8176914981a35b95a78
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-19 18:42:15 +0530

    music_player

--- src/ui-component/music-player/MusicPlayer.js
+++ src/ui-component/music-player/MusicPlayer.js
@@ -3,23 +3,25 @@ import { Box, makeStyles } from '@material-ui/core';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import { useSelector } from 'react-redux';
+import { BACKEND_HOST } from '../../api';
 
+import './styles.css'
+import constant from '../../themes/constant';
 const useStyles = makeStyles((theme) => ({
-    musicPlayer: {
-        position: 'fixed',
-        bottom: '-100%',
-        left: 0,
-        width: '100%',
-        backgroundColor: '#5e35b1',
-        boxShadow: '0 -2px 10px rgba(0,0,0,0.1)',
-        transition: 'bottom 0.5s ease-in-out',
-        zIndex: 1000,
-    },
-    musicPlayerVisible: {
-        bottom: 0,
-    },
+  musicPlayer: {
+    position: 'fixed',
+    bottom: '-100%', // Start off-screen
+    left: 0,
+    width: '100%',
+    backgroundColor: '#5e35b1',
+    boxShadow: '0 -2px 10px rgba(0,0,0,0.1)',
+    transition: 'bottom 0.5s ease-in-out',
+    zIndex: 1000, // Ensure it appears above other elements
+  },
+  musicPlayerVisible: {
+    bottom: 0, // End position
+  },
 }));
-
 const MusicPlayer = () => {
     const classes = useStyles();
     const { isVisible, currentSong } = useSelector((state) => state.musicPlayer);
@@ -30,7 +32,11 @@ const MusicPlayer = () => {
                 <div className='col-md-4'>
                     <div className='row'>
                         <div id="music-icon" className='col-md-6'>
-                            <img src={currentSong?.cover_img || "default-image-url"} alt="music-icon" />
+                        <img 
+                                src={currentSong?.cover_img ? `${BACKEND_HOST}${currentSong.cover_img}` : "default-image-url"} 
+                                alt="music-icon" 
+                  
+                            />
                         </div>
                         <div id='music-title' className='col-md-6'>
                             <b>{currentSong?.title || "Default Title"}</b>
